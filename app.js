60fd8ccdaaf96e06c271f06ea9f2cb9896bd3b1f
Larry X <papadocarock@gmail.com>
2020-09-30 13:42:49 -0400

    link html

--- app.js
+++ app.js
@@ -22,24 +22,20 @@ var notes = [{
 // =============================================================
 
 //Routes
-app.get("/", function(req, res) {
-
-    res.sendFile(path.join(__dirname, "view.html"));
-});
 
 app.get("/*", function(req, res) {
 
-    res.sendFile(path.join(__dirname, "view.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/notes", function(req, res) {
 
-    res.sendFile(path.join(__dirname, "view.html"));
+    res.sendFile(path.join(__dirname, "notes.html"));
 });
 
 app.get("/api/notes", function(req, res) {
 
-    res.sendFile(path.join(__dirname, "view.html"));
+    res.sendFile(path.join(__dirname, "notes.html"));
 });
 
 //post 
