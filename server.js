52ba18bf2c8525651f3321e995e9b7358f20434b
Larry X <papadocarock@gmail.com>
2020-10-01 22:48:56 -0400

    start

--- server.js
+++ server.js
@@ -2,21 +2,33 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+const { fstat } = require("fs");
+var Db = ("db/db.json");
+var fs = require("fs");
+const bodyParser = require('body-parser');
+
 
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 24000;
+var PORT = process.env.PORT || 4000;
+
+
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('/public'));
 
 //data
 
-var notes = [{
+const notes = [{
+    id: '',
+    body: ''
 
 }];
+
 
 // Routes
 // =============================================================
@@ -25,9 +37,10 @@ var notes = [{
 
 app.use(express.static("public"));
 
+//notes section
 app.get("/notes", function(req, res) {
-
     res.sendFile(path.join(__dirname, "public/notes.html"));
+
 });
 
 app.get("/*", function(req, res) {
@@ -36,49 +49,22 @@ app.get("/*", function(req, res) {
 });
 
 
-app.get("/api/notes", function(req, res) {
-
-    res.json(notes);
+app.post("/api/notes", function(req, res) {
+    console.log(req.body);
+    fs.readFile("./notes", "utf8", (err, data) => {
+        if (err) throw err;
+        console.log(data);
+        const notes = JSON.parse(data);
+        notes.forEach(req.body);
+        console.log(notes);
+    });
 });
 
-//post 
-app.post("api/notes", function(req, res) {
+//index section 
 
-});
 
-//Notes
-app.get("/api/notes/:notes", function(req, res) {
-    var chosen = req.params.notes;
-
-    console.log(notes);
-
-    for (var i = 0; i < notes.length; i++) {
-        if (chosen === notes[i].routeName) {
-            return res.json(notes[i]);
-        }
-    }
-
-    return res.json(false);
-});
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
-
-// Functions 
-
-const creatNote = newNotes => {
-
-};
-
-const saveNote = saveNotes => {
-
-};
-
-
-const deleteNote = deleteNotes => {
-
-};
-
-//server maps
